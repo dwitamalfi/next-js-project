411b5b32a9dc4b99552e34e5791109ce9a093b14
Alfi Dwitama <alfi.dwitama@bigio.id>
2023-06-06 13:54:44 +0700

    penambahan tailwind css

--- app/page.js
+++ app/page.js
@@ -4,7 +4,20 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-        <h1>Test Alfi</h1>
+      <div className="content-box">
+           <span className="timer">25:00</span>
+      </div>
+      <div className='flex space-between'>
+          <button type="button" class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 m-1">
+            Start
+          </button>
+          <button type="button" class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 m-1">
+            Pause
+          </button>
+          <button type="button" class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 m-1">
+            Reset
+          </button>
+      </div>
     </main>
   )
 }
