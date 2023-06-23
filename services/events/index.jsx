fdd785e2db9e4791779b0488ee41bb8ce16b0a18
Alfi Dwitama <alfi.dwitama@bigio.id>
2023-06-23 10:44:57 +0700

    penambahan update revalidate data

--- services/events/index.jsx
+++ services/events/index.jsx
@@ -2,7 +2,7 @@ export const getEvent = async (param) => {
     try {
         const response = await fetch(`https://dev.cms.dimulai.apps360.id/items/Event?page=${param.page}&limit=${[param.limit]}&search=${param.search}`,{
             method: 'GET',
-        },{ next: { revalidate: 60 } });
+        },{ next: { revalidate: true | 0 | 10 } });
 
         return response.json(); 
     } catch (error) {
